f61eb4b0512aa0600d9e1683df64f31fc7cec1bc
javakhishvili <158064572+javakhishvili@users.noreply.github.com>
2024-02-19 11:02:41 +0400

    Add files via upload

---
+++ Homework 2.py
@@ -0,0 +1,40 @@
+
+# 1. დაწერეთ პროგრამა რომელიც input მეთოდის საშუალებით მიიღებს 2 რიცხვს და დააბრუნებს ამ რიცხვებს შორის შესრულებული არითმეტიკული ოპერაციების შედეგებს (მიმატება, გამოკლება, გამრავლება, გაყოფა, ახარისხება).
+
+
+# x = eval(input("Enter first number: "))
+# y = eval(input("Enter second number: "))
+# print("The sum is", x + y)
+# print(f"The sum is {x + y}")
+# print(f"{x} + {y} = {x + y}")
+# print(f"{x} + {y} = {x + y}")
+# print(f"{x} * {y} = {x * y}")
+# print(f"{x} // {y} = {x // y}")
+# print(f"{x} ** {y} = {x ** y}")
+
+
+
+
+# 2. დაწერეთ პროგრამა რომბის ფართობის გამოსათვლელად. მომხმარებელს კლავიატურის გამოყენებით შეაქვს ორი დიაგონალის სიგრძე.
+
+# x = eval(input("Enter length: "))
+# y = eval(input("Enter width: "))
+# print(f"The area is {x * y}")
+
+
+# # 3. მომხმარებელის შეაქვს მეტრების რაოდენობა. დაბეჭდეთ შესაბამისი მნიშვნელობა სანტიმეტრებში, დეციმეტრებში, მილიმეტრებში, მილში.
+
+# x = eval(input("Enter M: "))
+# # y = eval(input("Enter second number: "))
+# print(f"The CM is", x * 100)
+# print(f"The DM is {x * 10}")
+# print(f"The MM is {x * 1000}")
+# print(f"The ML is {x * 0.00062137}")
+
+
+# 4. დაწერეთ პროგრამა, რომელიც ითვლის სამკუთხედის ფართობს. მომხმარებელს კონსოლიდან შეყავს სამკუთხედის სიმაღლისა და ფუძის მნიშვნელობა.
+
+# x = eval(input("Enter Height: "))
+# y = eval(input("Enter Base: "))
+# n = 1/2
+# print(f"The area is {n * x * y}")
